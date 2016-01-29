1a928dbc86738a6638b9ee843e67d29e5a3bd5f4
adam <adamstevenson121@gmail.com>
2016-01-29 17:58:38 -0500

    fixed graphical glitches on mac presumably caused by not clearing the buffer each update. see comments for more info

--- display.cpp
+++ display.cpp
@@ -11,6 +11,10 @@ Display::Display() {
     SCREEN_WIDTH * PIXEL_SIZE, SCREEN_HEIGHT * PIXEL_SIZE, SDL_WINDOW_SHOWN
   );
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  screen_rect.x = 0;
+  screen_rect.y = 0;
+  screen_rect.w = SCREEN_WIDTH;
+  screen_rect.h = SCREEN_HEIGHT;
 
 }
 
@@ -19,12 +23,19 @@ Display::~Display() {
   SDL_DestroyWindow(window);
 }
 
-void Display::update() {
-  SDL_Rect screen = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
-  update(screen);
+void Display::selfTest() {
+
 }
 
-void Display::update(const SDL_Rect &rect) {
+void Display::update() {
+  SDL_SetRenderDrawColor(renderer, 35, 35, 35, 255);
+  SDL_RenderClear(renderer);
+  update(screen_rect); //redraw entire screen
+}
+
+void Display::update(const SDL_Rect &rect) { //"dirty rectangles" method
+
+  //clear the rect to be updated
   SDL_SetRenderDrawColor(renderer, 35, 35, 35, 255);
   SDL_Rect clear = {
     rect.x * PIXEL_SIZE,
@@ -34,11 +45,17 @@ void Display::update(const SDL_Rect &rect) {
   };
   SDL_RenderFillRect(renderer, &clear);
 
+
   SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
   for(auto i = 0; i < rect.w * rect.h; i++) {
-    uint16_t pos = (rect.y + i / rect.w) * SCREEN_WIDTH + rect.x + i % rect.w;
+
+    uint16_t pos_x = rect.x + i % rect.w;
+    uint16_t pos_y = (rect.y + i / rect.w) * SCREEN_WIDTH;
+    
+    uint16_t pos = pos_x + pos_y;
+
     if(memory[pos]) {
-      SDL_Rect r = {
+      SDL_Rect r = { //construct "pixel" rect
         rect.x * PIXEL_SIZE + (i % rect.w) * PIXEL_SIZE,
         rect.y * PIXEL_SIZE + i / rect.w * PIXEL_SIZE,
         PIXEL_SIZE,
@@ -51,10 +68,8 @@ void Display::update(const SDL_Rect &rect) {
 }
 
 void Display::clear() {
-  SDL_SetRenderDrawColor(renderer, 35, 35, 35, 255);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
   memory.reset();
+  update();
 }
 
 uint8_t Display::blit(uint8_t* src, uint8_t size, uint8_t x, uint8_t y) {
@@ -67,8 +82,15 @@ uint8_t Display::blit(uint8_t* src, uint8_t size, uint8_t x, uint8_t y) {
       pixels = pixels << 1;
     }
   }
-  SDL_Rect dirty = {x, y, 8, size};
-  update(dirty);
+
+  //The "dirty rectangle" method is not used due to 
+  //strange behavior on the Mac platform. I believe this can
+  //be attributed to:
+  // "The backbuffer should be considered invalidated after
+  // each present" https://wiki.libsdl.org/SDL_RenderPresent#Remarks
+  //If anyone knows anything more, please let me know
+  //SDL_Rect dirty = {x, y, 8, size};
+  update(); 
   return pixel_cleared ? 1 : 0;
 }
 
--- display.h
+++ display.h
@@ -11,13 +11,15 @@ public:
   void update();
   void update(const SDL_Rect &rect);
   void clear();
+  void selfTest();
   uint8_t blit(uint8_t* src, uint8_t size, uint8_t x, uint8_t y);
 
 private:
 
   static const uint8_t SCREEN_WIDTH = 64;
   static const uint8_t SCREEN_HEIGHT = 32;
-  static const uint8_t PIXEL_SIZE = 10;
+  static const uint8_t PIXEL_SIZE = 12;
+  SDL_Rect screen_rect;
 
   SDL_Window *window;
   SDL_Renderer *renderer;
